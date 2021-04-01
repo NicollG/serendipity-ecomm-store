dc923a7b4ac01d70bc7871d47a1979c36982c8d8
nicollg <nicoll.g4@gmail.com>
2021-04-01 13:46:18 -0500

    more changes

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
-import { initiateCheckOut} from '../lib/payments.js';
 import products from '../products.json';
 
 
 export default function Home() {
-  console.log('products', products);
   return (
     <div className={styles.container}>
       <Head>
