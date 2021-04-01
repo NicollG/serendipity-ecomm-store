40e4256ee4e153ecfc2ede0a492075b83790f921
nicollg <nicoll.g4@gmail.com>
2021-04-01 12:54:44 -0500

    serendipity ecomm store

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,42 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import products from '../products.json';
+
 
 export default function Home() {
+  console.log('products', products);
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Serendipity</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         Welcome to Serendipity
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          The best place for your kids development!! 
+          Let's get started 
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className={styles.grid}>
+          {products.map(product => {
+            const {id, tittle, price, description, image} = product;
+            return (
+              <li key={id} className={styles.card}>
+                <a href="#" >
+                  <img src={image} alt={tittle}/>
+                  <h3>{ tittle }</h3>
+                  <p>${ price }</p>
+                  <p>{ description }</p>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
